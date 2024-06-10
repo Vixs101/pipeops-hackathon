a0f12f284e5df3b3760a8fa6f768d9a5de945470
liz-me <grantedbychoice@gmail.com>
2024-06-10 17:14:49 +0100

    create react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     
+        <p className='text-red-500'>
+          Health is wealth.
+        </p>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
