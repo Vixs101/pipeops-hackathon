9a62c3d0be91fd327f89762972e8f3e4f41ff610
Kayode Raimi <kayode.raimi123@gmail.com>
2024-06-13 22:35:16 +0100

    mongodb

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const express = require('express')
 const app = express()
-const PORT = 8080
+require('dotenv').config()
+const cors = require('cors')
+const PORT = process.env.port
+const db = require('./data/db')
 
-
+db.connectToMongoDB()
+app.use(cors())
 
 app.use(express.json())
 
 
 app.get('/', (req, res) => {
     res.json({message: 'Welcome Home'})
+    console.log("Home")
 })
 
 
 app.listen(PORT, (req, res) => {
     console.log(`Listening on port ${PORT}`)
+    console.log(process.env.MONGODB_URL)
 })
 
