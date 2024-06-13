f737b7d8614fd629fd16657ecd5909c67c17428b
Liz-me <grantedbychoice@gmail.com>
2024-06-13 18:55:53 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App h-full">
       <Navbar />
+      <Footer />
     </div>
   );
 }
