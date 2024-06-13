c7afef646915f7daa51efd708e8d1771eb3eca5a
Liz-me <grantedbychoice@gmail.com>
2024-06-13 19:20:02 +0100

    dashboard

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,20 @@
+// src/pages/Dashboard.js
+
+import React from 'react';
+
+import Sidebar from '../components/Sidebar';
+
+const Dashboard = () => {
+  return (
+      <div className="flex flex-col min-h-screen">
+        <div className="flex flex-grow">
+          <Sidebar />
+          <div className="flex-grow bg-gray-100 p-6">
+           
+          </div>
+        </div>
+      </div>
+  );
+};
+
+export default Dashboard;
